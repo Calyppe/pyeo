a20730d6251a79132e5a3bb3329c346d6364303c
polybispo <polybispo@gmail.com>
2018-04-09 14:19:19 +0100

    added csv_read.py

---
+++ pyeo/csv_read.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 11:26:54 2018
+
+@author: localadmin1
+"""
+
+import csv
+with open('lc_classes.csv', 'r') as csv_file:
+   csv_reader = csv.DictReader(csv_file)
+   
+
+
