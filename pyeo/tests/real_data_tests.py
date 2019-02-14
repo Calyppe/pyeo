c1f5c9e068dbc81c09bcff6515b484ec45cb14bd
John Roberts <jfr10@le.ac.uk>
2019-02-14 17:20:30 +0000

    Implementing apply_mask=True for classify_image

--- pyeo/tests/real_data_tests.py
+++ pyeo/tests/real_data_tests.py
@@ -313,6 +313,21 @@ def test_classification():
     assert not np.all(image_array == 0)
 
 
+@pytest.mark.slow
+def test_masked_classification():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    try:
+        os.remove("test_outputs/class_20180103T172709_20180319T172021.tif")
+    except FileNotFoundError:
+        pass
+    pyeo.classify_image("test_data/T13QFB_20180103T172709_20180329T171921.tif", "test_data/manantlan_v1.pkl",
+                        "test_outputs/class_T13QFB_20180103T172709_20180319T172021.tif", num_chunks=4, apply_mask=True)
+    image = gdal.Open("test_outputs/class_T13QFB_20180103T172709_20180319T172021.tif")
+    assert image
+    image_array = image.GetVirtualMemArray()
+    assert not np.all(image_array == 0)
+
+
 def test_mask_combination():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     masks = [r"test_data/"+mask for mask in os.listdir("test_data") if mask.endswith(".msk")]
