9e0700246b4cae9552cba94b4253a9304ca9d563
YaqingGou <yg137@leicester.ac.uk>
2019-05-10 17:14:22 +0100

    current version with planet downloading... may have a problem after the API change

--- pyeo/apps/change_detection/planet_change_detection.py
+++ pyeo/apps/change_detection/planet_change_detection.py
@@ -5,58 +5,58 @@ import argparse
 import configparser
 import os
 
-# Load config
 
-parser = argparse.ArgumentParser(description='Cla')
-parser.add_argument('--conf', dest='config_path', action='store', default=r'change_detection.ini',
-                    help="Path to the .ini file specifying the job.")
-args = parser.parse_args()
+if __name__ == "__main__":
+    # Load config
+    parser = argparse.ArgumentParser(description='Cla')
+    parser.add_argument('--conf', dest='config_path', action='store', default=r'change_detection.ini',
+                        help="Path to the .ini file specifying the job.")
+    args = parser.parse_args()
 
-conf = configparser.ConfigParser()
-conf.read(args.config_path)
-api_key_path = conf['planet']['api_key']
-project_root = conf['planet']['root_dir']
-aoi_path = conf['planet']['aoi_path']
-start_time = conf['planet']['start_date']
-end_time = conf['planet']['end_date']
-log_path = conf['planet']['log_path']
-model_path = conf['planet']['model']
+    conf = configparser.ConfigParser()
+    conf.read(args.config_path)
+    api_key_path = conf['planet']['api_key']
+    project_root = conf['planet']['root_dir']
+    aoi_path = conf['planet']['aoi_path']
+    start_time = conf['planet']['start_date']
+    end_time = conf['planet']['end_date']
+    log_path = conf['planet']['log_path']
+    model_path = conf['planet']['model']
 
+    #create folder structure
+    pyeo.create_file_structure(project_root)
+    log = pyeo.init_log(log_path)
 
+    planet_image_path = os.path.join(project_root, r"images/planet")
+    stacked_image_path = os.path.join(project_root, r"images/stacked")
+    catagorised_image_path = os.path.join(project_root, r"output/categories")
+    probability_image_path = os.path.join(project_root, r"output/probabilities")
 
-pyeo.create_file_structure(project_root)
-log = pyeo.init_log(log_path)
+    api_key = api_key_path
 
-planet_image_path = os.path.join(project_root, r"images/planet")
-stacked_image_path = os.path.join(project_root, r"images/stacked")
-catagorised_image_path = os.path.join(project_root, r"output/categories")
-probability_image_path = os.path.join(project_root, r"output/probabilities")
+    # Download
+    pyeo.planet_query(aoi_path, start_time, end_time, out_path=planet_image_path,
+                      api_key=api_key_path, item_type = "PSScene4Band" , search_name="auto", asset_type="analytic", threads=5)
 
-api_key = api_key_path  # For now
-
-# Download two images
-
-pyeo.planet_query(aoi_path, start_time, end_time, out_path=planet_image_path,
-                  api_key=api_key_path, item_type = "PSScene4Band" , search_name="auto", asset_type="analytic", threads=5)
-#image_1_path = pyeo.download_planet_image_on_day(aoi_path, date_1, planet_image_path, api_key)
-#image_2_path = pyeo.download_planet_image_on_day(aoi_path, date_2, planet_image_path, api_key)
-
-# Save RGB
+    # export shp
 
 
-# Do classification
-images_to_stack = [os.path.join(dirpath,f)
-                       for dirpath, dirnames, files in os.walk(planet_image_path)
-                       for f in files if f.endswith('.tif')]
+    # mosaic
 
-stacked_image_path = pyeo.stack_images(images_to_stack, stacked_image_path)
-# pyeo.classify_image(stacked_image_path, model_path=model_path)
 
-# Do the imshow transitive closure trick
+    # Do classification
+    images_to_stack = [os.path.join(dirpath,f)
+                           for dirpath, dirnames, files in os.walk(planet_image_path)
+                           for f in files if f.endswith('.tif')]
 
-# Polygonise
+    stacked_image_path = pyeo.stack_images(images_to_stack, stacked_image_path)
+    # pyeo.classify_image(stacked_image_path, model_path=model_path)
 
-# Burn
+    # Do the imshow transitive closure trick
 
-# Resample
+    # Polygonise
+
+    # Burn
+
+    # Resample
 
